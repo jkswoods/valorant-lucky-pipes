29620297855729ff1a7c007a0c3e2b52e8fa5a8f
Joshua Stevenson-Woods <jkwoods@live.co.uk>
2021-03-20 03:38:53 +0000

    Fix valorant logo font

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,9 +2,9 @@
     <div class="bg-valorant-red h-screen relative">
         <div class="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
             <div class="text-center">
-                <h2 class="text-base font-semibold text-valorant-white tracking-wide uppercase font-display">Valorant</h2>
+                <h2 class="text-base font-semibold text-valorant-white tracking-wide font-display">vAlor<span class="tracking-normal">an</span>t</h2>
                 <p class="mt-1 text-4xl font-extrabold text-valorant-white sm:text-5xl sm:tracking-tight lg:text-6xl font-display">Church of the lucky pipe</p>
-                <p class="mt-10 text-base font-extrabold text-gray-900 sm:text-base sm:tracking-tight lg:text-xl font-display">Pledge allegiance soon...</p>
+                <p class="mt-10 text-base font-extrabold text-gray-900 sm:text-base sm:tracking-tight lg:text-xl font-display">Pledge Allegi<span class="tracking-normal">an</span>ce soon...</p>
             </div>
         </div>
         <div class="h-3 w-3 absolute bottom-0 right-0 bg-gray-900"/>
